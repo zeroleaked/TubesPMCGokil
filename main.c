d06f6e48c93f33d5ffe8998e29e94e75834e8644
rayleigh18 <sitanggangirfan271000@gmail.com>
2020-03-17 21:10:29 +0700

    Merapikan Kode

--- main.c
+++ main.c
@@ -14,23 +14,10 @@
 #include <string.h>
 #include <stdlib.h>
 #include "matrices.h"
+#include "configuration.h"
 
 int main(){
-    int n ;
-    float **matrices;
-    
-    scanf("%d", &n);
-    int i;
-    matrices = (float**) malloc(n * sizeof(float*));
 
-    for ( i = 0; i < n; i++){
-        matrices[i] = (float*)malloc(n * sizeof(float));
-    }
-    inputMatrix(matrices,n);
-    printMatrix(matrices,n);
 
-    float res = luDecomposition(matrices, n) ;
-
-    printf("%f" , res);
     return 0;
 }
