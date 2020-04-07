f6f8d0d0e3c4cf34f3bab0cfcaa80498bf469301
zir <fakegags@gmail.com>
2020-04-08 02:17:30 +0700

    added comments

--- Output/output.c
+++ Output/output.c
@@ -31,33 +31,32 @@ void addHeaderToFile(
   FILE **fptr
 ) {
   fprintf(*fptr, "t,");
+  // print nodes
   for (int i = 0; i < node_array_length; i++) {
     if (node_array[i] != ground && node_array[i] > 0)
       fprintf(*fptr, "node %d (Volt),", node_array[i]);
   }
+  // print header tegangan komponen
   for (int i = 0; i < component_array_length; i++) {
     if ( component_array[i].type == 'v') {
       fprintf(*fptr, "V(C) (Volt),");
-      i++;
-      continue;
+      i++;\
     } else if ( component_array[i].type == 'i' ) {
       fprintf(*fptr, "V(L) (Volt),");
       i++;
-      continue;
-    }
-    fprintf(*fptr, "V(%c) (Volt),", component_array[i].type);
+    } else
+      fprintf(*fptr, "V(%c) (Volt),", component_array[i].type);
   }
+  // print header arus komponen
   for (int i = 0; i < component_array_length; i++) {
     if ( component_array[i].type == 'v') {
       fprintf(*fptr, "I(C) (Amp),");
       i++;
-      continue;
     } else if ( component_array[i].type == 'i') {
       fprintf(*fptr, "I(L) (Amp),");
       i++;
-      continue;
-    }
-    fprintf(*fptr, "I(%c) (Amp),", component_array[i].type);
+    } else
+      fprintf(*fptr, "I(%c) (Amp),", component_array[i].type);
   }
   fprintf(*fptr, "\n");
 }
@@ -87,14 +86,17 @@ void addComponentVoltagesToFile(
   FILE **fptr
 ) {
   for (int i = 0; i < component_array_length; i++) {
+    // kasus kapasitor
     if ( component_array[i].type == 'v' ) {
       fprintf(*fptr, "%f,",
+        // sumber tegangan ditambah tegangan resistor
         solved_array[offset + i] + solved_array[offset + i + 1]);
-      i++;
+      i++; // skip resistor
       continue;
     }
+    // kasus induktor, tegangan sumber arus dan resistor sama (paralel)
     else if ( component_array[i].type == 'i' ) {
-      i++;
+      i++; // skip resistor
     }
     fprintf(*fptr, "%f,", solved_array[offset + i]);
   }
@@ -108,14 +110,17 @@ void addComponentCurrentsToFile(
   FILE **fptr
 ) {
   for (int i = 0; i < component_array_length; i++) {
+    // kasus induktor
     if ( component_array[i].type == 'i' ) {
       fprintf(*fptr, "%f,",
+        // sumber arus ditambah arus resistor
         solved_array[offset + i] + solved_array[offset + i + 1]);
-      i++;
+      i++; // skip resistor
       continue;
     }
+    // kasus kapasitor, arus sumber tegangan dan resistor sama (seri)
     else if ( component_array[i].type == 'v' ) {
-      i++;
+      i++; // skip resistor
     }
     fprintf(*fptr, "%f,", solved_array[offset + i]);
   }
