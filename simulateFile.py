3dd14a577b268b8b2a1a7b87154e7b6efd106111
zir <fakegags@gmail.com>
2020-04-30 04:22:06 +0700

    layouting

--- simulateFile.py
+++ simulateFile.py
@@ -18,7 +18,6 @@ class addFrame(Tk.Toplevel):
     def __init__(self, original, frameName):
         self.original_frame = original
         Tk.Toplevel.__init__(self)
-        self.geometry("400x300")
         self.title(frameName)
         self.Row = 0
 
@@ -40,32 +39,35 @@ class simulateCircuit(Tk.Toplevel):
 
         self.Row = 0
         Tk.Toplevel.__init__(self)
-        self.geometry("800x600")
+        # self.geometry("800x600")
         self.title("Simulate")
 
         rowStart = self.getRow()
+        print rowStart
 
         timestartEntry = Tk.Entry(self)
         timendEntry = Tk.Entry(self)
 
-        timestartLabel=Tk.Label(self,text = "Time Start :")
-        timeendLabel=Tk.Label(self,text = "Time End :")
+        timestartLabel=Tk.Label(self,text = "Start Time (s):")
+        timeendLabel=Tk.Label(self,text = "End Time (s):")
 
-        timestartEntry.grid(row = rowStart, column = 2)
-        timestartLabel.grid(row=rowStart, column = 1)
+        timestartEntry.grid(row = rowStart, column = 1, columnspan=2, padx=5, pady=5)
+        timestartLabel.grid(row=rowStart, column = 0, padx=5, pady=5, sticky="E")
 
-        timendEntry.grid(row=rowStart+1, column = 2)
-        timeendLabel.grid(row=rowStart+1, column = 1)
+        timendEntry.grid(row=rowStart+1, column = 1, columnspan=2, padx=5, pady=5)
+        timeendLabel.grid(row=rowStart+1, column = 0, padx=5, pady=5, sticky="E")
         self.getRow()
 
+        button_row = self.getRow()
+
         btngrou =Tk.Button(self, text="Add Ground", command=self.addGround)
-        btngrou.grid(row = self.getRow())
+        btngrou.grid(row = button_row, column = 1, padx=5, pady=5)
 
         btnsim = Tk.Button(self, text="Simulate", command=lambda :self.simulateNow(timestartEntry, timendEntry))
-        btnsim.grid(row = self.getRow(), column = 0)
+        btnsim.grid(row = button_row, column = 2, padx=5, pady=5)
 
-        btn = Tk.Button(self, text="Close", command=self.onClose)
-        btn.grid(row = self.getRow(), column = 0)
+        btn = Tk.Button(self, text="Cancel", command=self.onClose)
+        btn.grid(row = button_row, column = 0, padx=5, pady=5)
     def hide(self):
         self.withdraw()
 
@@ -88,18 +90,18 @@ class simulateCircuit(Tk.Toplevel):
         grouValueEntry = Tk.Entry(otherFrame)
 
         rowStart = otherFrame.getRow()
-        grouValueLabel=Tk.Label(otherFrame,text = "Nilai Node Ground:")
+        grouValueLabel=Tk.Label(otherFrame,text = "Ground Node:")
 
-        grouValueLabel.grid(row=rowStart, column = 1)
+        grouValueLabel.grid(row=rowStart, column = 1, padx=5, pady=5)
 
-        grouValueEntry.grid(row=rowStart, column = 2)
+        grouValueEntry.grid(row=rowStart, column = 2, columnspan=2, padx=5, pady=5)
 
         submit = Tk.Button(otherFrame, text = "Submit")
         cancel = Tk.Button(otherFrame, text = "Cancel")
         submit['command'] =lambda binst =submit, binst2 = cancel:self.putGround(1,grouValueEntry,otherFrame)
         cancel['command'] = lambda binst = cancel,  binst2 = submit:self.putGround(0,grouValueEntry,otherFrame)
-        cancel.grid(row = rowStart+4, column = 4)
-        submit.grid(row = rowStart+4, column = 5)
+        cancel.grid(row = rowStart+4, column = 2, padx=5, pady=5)
+        submit.grid(row = rowStart+4, column = 3, padx=5, pady=5)
 
     def putGround(self, state ,grouValueEntry, otherFrame):
         if state == 1:
